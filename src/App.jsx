90b4e4b69aead2cb53fdf08343099eca0ecbbcd9
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-08-24 08:56:27 -0400

    feat: use hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./tailwind.css";
 import "./global.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import About from "./screens/About";
 import Resume from "./screens/Resume";
