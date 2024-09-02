bae0ae9245d4e0365fb4836ce608721835d406a5
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-09-02 11:41:10 -0400

    feat: experiences compatibility

--- m.sh
+++ m.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Loop through each gallery folder within the projects directory
 for gallery_dir in ./public/assets/projects/*/gallery; do
-    # Check if the directory exists
     if [ -d "$gallery_dir" ]; then
-        # Initialize the JSON array
         json_array="["
         
-        # Find all image files with extensions png, jpg, jpeg in the current gallery directory
         images=$(find "$gallery_dir" -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" \) -exec basename {} \;)
         
-        # Loop through each image file and append it to the JSON array
         first=true
         for image in $images; do
             if [ "$first" = true ]; then
@@ -21,10 +16,30 @@ for gallery_dir in ./public/assets/projects/*/gallery; do
             json_array+="\"$image\""
         done
         
-        # Close the JSON array
         json_array+="]"
         
-        # Write the JSON array to gallery.json in the current gallery folder
+        echo "$json_array" > "$gallery_dir/gallery.json"
+    fi
+done
+
+for gallery_dir in ./public/assets/experiences/*/gallery; do
+    if [ -d "$gallery_dir" ]; then
+        json_array="["
+        
+        images=$(find "$gallery_dir" -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" \) -exec basename {} \;)
+        
+        first=true
+        for image in $images; do
+            if [ "$first" = true ]; then
+                first=false
+            else
+                json_array+=", "
+            fi
+            json_array+="\"$image\""
+        done
+        
+        json_array+="]"
+        
         echo "$json_array" > "$gallery_dir/gallery.json"
     fi
 done
