c617b964de5377ae5fab742c96a98ba1f82dc48e
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2024-09-02 15:13:12 -0400

    feat:

--- src/constants.js
+++ src/constants.js
@@ -56,4 +56,4 @@ export const INDUSTRY_OPTIONS = [
   "Software",
 ];
 
-export const experienceIds = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17];
+export const experienceIds = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17, 18];
